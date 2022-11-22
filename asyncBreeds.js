ee82c206700b51717e0e7a98afc246b3aae7c6b4
Tai <Tai.Delisle@telus.com>
2022-11-22 16:07:12 -0700

    add cats files

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+const printBreedDesc = breed => {
+  console.log('Return Value: ', breed);
+}
+
+breedDetailsFromFile('Bombay', printBreedDesc);
